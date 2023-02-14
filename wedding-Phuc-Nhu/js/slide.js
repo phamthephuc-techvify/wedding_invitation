9e0bf8ee0fb0fb50dd4652f5eef62d18aa934281
kando <phuc.pham@techvify.jp>
2023-02-15 00:05:52 +0900

    change text slide

--- wedding-Phuc-Nhu/js/slide.js
+++ wedding-Phuc-Nhu/js/slide.js
@@ -1,9 +1,9 @@
 (function ($){
     $(document).ready(function(){
         let slider_content = `
-        <span>WELCOME TO OUR BEGINNING</span>
+        <span>SAVE THE DATE</span>
         <h4>to have and to hold<br>from this day forward</h4>
-        <p>28 - 08 - 2022</p>
+        <p>22 - 05 - 2023</p>
         `
         $('.slider-content--inner').html(slider_content)
     })
